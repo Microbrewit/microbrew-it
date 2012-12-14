c34c52aa8c5558096644aad1869e0af57fe39204
EivindEE <eivind.elseth@gmail.com>
2012-12-14 15:07:48 +0100

    Added function and route for /beer/add, Adds a beer to the triple store

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express'),
   routes = require('./routes'),
   user = require('./routes/user'),
   mail = require('./routes/mail'),
+  query = require('./routes/query'),
   http = require('http'),
   path = require('path');
 
@@ -34,6 +35,9 @@ app.configure('development', function () {
 app.get('/', routes.index);
 app.get('/mail', mail.mail);
 app.get('/users', user.list);
+app.get('/beer/add', query.insert);
+app.get('/beer/query', query.query);
+app.get('/beer/put', query.put);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
--- routes/query.js
+++ routes/query.js
@@ -1,5 +1,46 @@
-var http = require('http');
-var querystring = require('querystring');
+var http = require('http'),
+	querystring = require('querystring'),
+	host = 'microbrewit.thunemedia.no',
+	port = 8080,
+	createInsertString = function (options, callback) {
+		var prefixes = 'PREFIX mb:<http://www.microbrew.it/>',
+			insert = 'INSERT DATA { ';
+		if (!options.name) {
+			callback(new Error('Name required'));
+		} else {
+			insert += 'mb:' + options.name + ' rdf:type mb:Beer';
+			insert += '; mb:name "' + options.name + '"}';
+			console.log(prefixes + insert);
+			callback(null, encodeURIComponent(prefixes + insert));
+		}
+	};
+
+exports.insert = function (req, res) {
+	createInsertString({'name': 'Hansa'}, function (err, result) {
+		var options = {
+				'host': host,
+				'port': port,
+				'path': '/openrdf-sesame/repositories/microbrewit/statements?update=' + result,
+				'headers': {
+					'Content-Type': 'application/x-www-form-urlencoded'
+      				},
+				'method': 'POST'
+			},
+			request = http.request(options, function (response) {
+				res.writeHead(200, {'Content-Type' : 'text/plain'});
+			    res.write('STATUS: ' + response.statusCode);
+			    res.write('\nHEADERS: ' + JSON.stringify(response.headers));
+				res.end('\nDone');
+				console.log(response);
+		    });
+			request.on('error', function (e) {
+				console.log('problem with update request: ' + e.message);
+				res.writeHead(500, {'Content-Type' : 'text/plain'});
+				res.end(e.message);
+			});
+			request.end();
+	});
+} 
 
 exports.query = function (req, res) {
   var uri = '<' + req.param('uri') + '>';
@@ -10,11 +51,11 @@ exports.query = function (req, res) {
     repository = '/openrdf-sesame/repositories/microbrewit?query=',
     returnedJSON = '',
     options = {
-      host: 'microbrewit.thunemedia.no',
-      port: 8088,
-      path: repository + encodeURIComponent(prefix) + select,
-      headers: { accept: 'application/sparql-results+json'},
-      method: 'GET'
+      'host': host,
+      'port': port,
+      'path': repository + encodeURIComponent(prefix) + select,
+      'headers': { accept: 'application/sparql-results+json'},
+      'method': 'GET'
     },
 
     request = http.request(options, function (response) {
@@ -37,16 +78,16 @@ exports.query = function (req, res) {
 };
 
 exports.put = function (req, res) {
-  var name = req.param('name'),
+  var name = req.param.name || '',
     prefix = 'PREFIX ns:<http://www.example.org/#>',
     insertStart = 'INSERT DATA { ns:',
     insertEnd = ' rdf:type ns:Person}',
     update = encodeURIComponent(prefix + insertStart + name + insertEnd),
     options = {
-      host: 'microbrewit.thunemedia.no',
-      port: 8088,
-      path: '/openrdf-sesame/repositories/microbrewit/statements?update=' + update,
-      headers: {
+      'host': host,
+      'port': port,
+      'path': '/openrdf-sesame/repositories/microbrewit/statements?update=' + update,
+      'headers': {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       method: 'POST'
@@ -56,6 +97,7 @@ exports.put = function (req, res) {
 	    console.log('HEDERS: ' + JSON.stringify(response.headers));
       console.log('update request done');
     });
+  console.log(prefix + insertStart + name + insertEnd);
   request.on('error', function (e) {
     console.log('poblem with update request: ' + e.message);
   });
