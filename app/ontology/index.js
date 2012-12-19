5d80f0e3f56624745e95294158a98f3198a08834
John Fredrik Asphaug <john-f@online.no>
2012-12-19 14:48:47 +0100

    Added all predicates from bee register into ontology object

--- app/ontology/index.js
+++ app/ontology/index.js
@@ -1,9 +1,25 @@
 //Ontology export
-var baseURI = 'http://www.microbrew.it/';
+var baseURI = 'http://ontology.microbrew.it/';
 exports.mb = {
-	beerName : ' <' + baseURI + 'Beer/name> ',
-	beerStyle : ' <' + baseURI + 'beer/hasStyle> ',
-	abv : ' <' + baseURI + 'beer/abv> ',
-	beerOrigin : ' <' + baseURI + 'beer/origin> ',
-
+//beer
+	beer  : ' <' + baseURI + 'beer> ',
+	name : ' <' + baseURI + 'name> ',
+	style : ' <' + baseURI + 'hasStyle> ',
+	abv : ' <' + baseURI + 'abv> ',
+	origin : ' <' + baseURI + 'origin> ',
+	bottle : ' <' + baseURI + 'bottle> ',
+	image : ' <' + baseURI + 'image> ',
+	label : ' <' + baseURI + 'label> ',
+	comment : ' <' + baseURI + 'comment> ',
+	description : ' <' + baseURI + 'description> ',
+	servingType : ' <' + baseURI + 'servingType> ',
+	glassType : ' <' + baseURI + 'glassType> ',
+	ebu : ' <' + baseURI + 'ebu> ',
+	aroma : ' <' + baseURI + 'aroma> ',
+	appearance : ' <' + baseURI + 'appearance> ',
+	mouthfeel : ' <' + baseURI + 'mouthfeel> ',
+	ebc : ' <' + baseURI + 'ebc> ',
+	colour : ' <' + baseURI + 'colour> ',
+	barcode : ' <' + baseURI + 'barcode> ',
+	brewedBy : ' <' + baseURI + 'brewedBy> ',
 };
