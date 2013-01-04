93e7267e00b939e3c77d04d151de26c458b0dd7c
Torstein Thune <torstein@gmail.com>
2013-01-04 14:46:23 +0100

    Changed variable in BeerURI in exports.query to fit with changes in app.js

--- routes/index.js
+++ routes/index.js
@@ -67,16 +67,22 @@ exports.insert = function (req, res) {
 };
 
 exports.query = function (req, res) {
-	var beerURI = '<http://www.microbrew.it/beer/' + req.params.beer + '>';
+	var beerURI = '<http://www.microbrew.it/beer/' + req.params.searchTerms + '>';
+
 	console.log(beerURI);
 	query.select(beerURI, function (error, result) {
 		if (error) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.end(error.message);
 		} else {
-			//res.render('beer', result);
-			res.writeHead(200, {'Content-Type' : 'application/json'});
-			res.end(JSON.stringify(result));
+			if(result.results.bindings.length == 1) {
+				res.render('beer', result);
+			} else {
+				res.writeHead(500, {'Content-Type': 'text/plain'});
+				res.end('Exception: Mothefucking internal shit with arrays error'); // TODO: add error view
+			}
+			// res.writeHead(200, {'Content-Type' : 'application/json'});
+			// res.end(JSON.stringify(result));
 		}
 	});
 };
