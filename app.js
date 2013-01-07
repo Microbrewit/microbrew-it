0f10abc89bb2c32b281e8c6b405a5ea8c1b8c358
EivindEE <eivind.elseth@gmail.com>
2013-01-07 15:33:45 +0100

    Removed unused middleware

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.configure(function () {
 	app.use(express.cookieParser('your secret here'));
 	app.use(express.session());
 	app.use(app.router);
-//	app.use(require('stylus').middleware(__dirname + '/public'));
+
 	app.use(express.static(path.join(__dirname, 'public')));
 });
 
