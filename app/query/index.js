d36269703ba7a30d8c602fe0799b4cd6d978df23
EivindEE <eivind.elseth@gmail.com>
2013-01-07 13:52:38 +0100

    Moved responsibility for http connection to triplestore to own package

--- app/query/index.js
+++ app/query/index.js
@@ -20,28 +20,28 @@ var http = require('http'),
 				console.log('===== RECEIVED URI TO INSERT INTO =====');
 			}
 
-			insert += options.brewery ? mb.brewedBy + ' <http://www.microbrew.it/Brewery/' + encodeURIComponent(options.brewery) + '>' : '';
-			insert += options.styles ?  mb.style + ' "' + options.styles + '";' : '';
-			insert += options.abv ?  mb.abv + ' "' + options.abv + '";' : '';
-			insert += options.origin ?  mb.origin + ' "' + options.origin + '";' : '';
-			insert += options.image ?  mb.image + ' "' + options.image + '";' : '';
-			insert += options.bottle ?  mb.bottle + ' "' + options.bottle + '";' : '';
-			insert += options.label ?  mb.label + ' "' + options.label + '";' : '';
-			insert += options.comment ?  mb.comment + ' "' + options.comment + '";' : '';
-			insert += options.description ?  mb.description + '"' + options.description + '";' : '';
-			insert += options.servingtype ?  mb.servingType + '"' + options.servingtype + '";' : '';
-			insert += options.glasstype ?  mb.glassType + ' "' + options.glasstype + '";' : '';
-			insert += options.ibu ? mb.ibu + ' "' + options.ibu + '";' : '';
-			insert += options.aroma ? mb.aroma + ' "' + options.aroma + '";' : '';
-			insert += options.appearance ?  mb.appearance + ' "' + options.appearance + '";' : '';
-			insert += options.mouthfeel ?  mb.mouthfeel + ' "' + options.mouthfeel + '";' : '';
-			insert += options.colour ? mb.colour + ' "' + options.colour + '";' : '';
-			insert += options.barcode ? mb.barcode + ' "' + options.barcode + '";' : '';
-			insert += options.ebc ? mb.ebc + '"' + options.ebc + '";' : '';
+			insert += options.brewery ? ' ; ' + mb.brewedBy + ' <http://www.microbrew.it/Brewery/' + encodeURIComponent(options.brewery) + '> ' : '';
+			insert += options.styles ?  ' ; ' + mb.style + ' "' + options.styles + '"': '';
+			insert += options.abv ?  ' ; ' + mb.abv + ' "' + options.abv + '"': '';
+			insert += options.origin ?  ' ; ' + mb.origin + ' "' + options.origin + '"': '';
+			insert += options.image ?  ' ; ' + mb.image + ' "' + options.image + '"': '';
+			insert += options.bottle ?  ' ; ' + mb.bottle + ' "' + options.bottle + '"': '';
+			insert += options.label ?  ' ; ' + mb.label + ' "' + options.label + '"': '';
+			insert += options.comment ? ' ; ' +  mb.comment + ' "' + options.comment + '"': '';
+			insert += options.description ?  ' ; ' + mb.description + '"' + options.description + '"': '';
+			insert += options.servingtype ?  ' ; ' + mb.servingType + '"' + options.servingtype + '"': '';
+			insert += options.glasstype ?  ' ; ' + mb.glassType + ' "' + options.glasstype + '"': '';
+			insert += options.ibu ? ' ; ' + mb.ibu + ' "' + options.ibu + '"': '';
+			insert += options.aroma ? ' ; ' + mb.aroma + ' "' + options.aroma + '"': '';
+			insert += options.appearance ?  ' ; ' + mb.appearance + ' "' + options.appearance + '"': '';
+			insert += options.mouthfeel ?  ' ; ' + mb.mouthfeel + ' "' + options.mouthfeel + '"': '';
+			insert += options.colour ? ' ; ' + mb.colour + ' "' + options.colour + '"' : '';
+			insert += options.barcode ? ' ; ' + mb.barcode + ' "' + options.barcode + '"': '';
+			insert += options.ebc ? ' ; ' + mb.ebc + '"' + options.ebc + '"': '';
 			insert += options.brewery ? '.  <http://www.microbrew.it/Brewery/' + encodeURIComponent(options.brewery) + '>' + mb.name + ' "' + options.brewery + '"' : '';
 			insert +=  ' }';
 			console.log("INSERT: " + insert);
-			callback(null, encodeURIComponent(insert));
+			callback(null, insert);
 		}
 	},
 	options = {
@@ -69,31 +69,19 @@ exports.insert = function (beer, callback) {
 		if (err) {
 			callback(err);
 		} else {
-			options.path = config.ts.path.insert;
-			var request = http.request(options, function (response) {
-					response.on('end', function () {
-						if (response.statusCode !== 204) {
-							callback(new Error('Insertion was not accepted'));
-						} else {
-							callback(null, {'statusCode': response.statusCode, 'query' : result});
-						}
-					});
-					response.on('data', function (data) {
-					});
-
-				});
-			request.on('error', function (e) {
-				callback(new Error(e.message));
+			ts.insert(result, function (error, result) {
+				if (error) {
+					callback(error);
+				} else {
+					callback(null, result);
+				}
 			});
-			request.end('update=' + result);
 		}
 	});
 };
 
 exports.select = function (beerName, callback) {
-	var returnedJSON = '',
-		request,
-		select;
+	var select;
 	select = 'SELECT * WHERE { ?uri ' + mb.name + ' "' + beerName + '" .';
 	select += ' ?uri' + mb.name + ' ?name .';
 	select += ' OPTIONAL { ?uri' + mb.style + ' ?style} . ';
@@ -117,25 +105,14 @@ exports.select = function (beerName, callback) {
 	select += ' OPTIONAL { ?brewedBy' + mb.name  + '?breweryName} . ';
 	select += '}';
 
-	console.log(select);
-	options.path = config.ts.path.query;
-	options.headers.accept = 'application/sparql-results+json';
-	request = http.request(options, function (response) {
-		console.dir(response);
-		response.setEncoding('utf8');
-		response.on('data', function (chunk) {
-			returnedJSON += chunk;
-		});
-		response.on('end', function () {
-			var json = JSON.parse(returnedJSON);
-			console.log(JSON.stringify(json)); // TODO: remove (used for bugfix)
-			callback(null, json);
-		});
+	ts.select(select, function (err, result) {
+		if (err) {
+			callback(err);
+		} else {
+			callback(null, result);
+		}
 	});
-	request.on('error', function (e) {
-		callback(new Error(e.message));
-	});
-	request.end('query=' + encodeURIComponent(select));
+
 };
 
 exports.findBrewery = function (breweryName, callback) {
@@ -145,24 +122,11 @@ exports.findBrewery = function (breweryName, callback) {
 	select = 'SELECT * WHERE {?breweryURI ' + mb.name + ' "' + breweryName + '" .';
 	select += ' OPTIONAL { ?breweryURI ' + mb.name + ' ?name} . ';
 	select += '}';
-
-	console.log(select);
-	options.path = config.ts.path.query;
-	options.headers.accept = 'application/sparql-results+json';
-	request = http.request(options, function (response) {
-		response.setEncoding('utf8');
-		response.on('data', function (chunk) {
-			console.log(chunk);
-			returnedJSON += chunk;
-		});
-
-		response.on('end', function () {
-			var json = JSON.parse(returnedJSON);
-			callback(null, json);
-		});
+	ts.select(select, function (err, result) {
+		if (err) {
+			callback(err);
+		} else {
+			callback(null, result);
+		}
 	});
-	request.on('error', function (e) {
-		callback(new Error(e.message));
-	});
-	request.end('query=' + encodeURIComponent(select));
 };
--- app/tripplestore/index.js
+++ app/tripplestore/index.js
@@ -1,28 +1,76 @@
 var http = require('http'),
 	config = require('../config'),
-	mb = require('../ontology').mb;
-exports.ask = function (query, callback) {
-	var options = {
+	mb = require('../ontology').mb,
+	util = require('../util'),
+	opts = {
 		'host': config.ts.host,
 		'port': config.ts.port,
-		'path': config.ts.path.query,
 		
 		'headers': {
 			'Content-Type': 'application/x-www-form-urlencoded',
-			'accept' : 'text/boolean'
 		},
 		'method': 'POST'
-	},
-	data = '',
-	request = http.request(options, function (response) {
-			response.on('data', function (chunk) {
-				data += chunk;
+	};
+exports.ask = function (query, callback) {
+	var options = util.beget(opts);
+	options.headers.accept = 'text/boolean';
+	options.path =  config.ts.path.query;
+	var data = '',
+		request = http.request(options, function (response) {
+				response.on('data', function (chunk) {
+					data += chunk;
 
-			});
-			response.on('end', function () {
-				callback(null, data);
-			});
+				});
+				response.on('end', function () {
+					callback(null, data);
+				});
 		});
+	request.on('error', function (e) {
+		callback(new Error(e.message));
+	});
+	request.end('query=' + encodeURIComponent(query));
+};
+
+exports.insert = function (query, callback) {
+	var options = util.beget(opts);
+	options.path = config.ts.path.insert;
+
+	var request = http.request(options, function (response) {
+			response.on('end', function () {
+				if (response.statusCode !== 204) {
+					callback(new Error('Insertion was not accepted (' + response.statusCode + ')'));
+				} else {
+					callback(null, {'statusCode': response.statusCode, 'query' : query});
+				}
+			});
+			response.on('data', function (data) {
+			});
+
+		});
+
+	request.on('error', function (e) {
+		callback(new Error(e.message));
+	});
+	console.log('update=' + encodeURIComponent(query));
+	request.end('update=' + encodeURIComponent(query));
+};
+
+exports.select = function (query, callback) {
+	var options = util.beget(opts),
+		returnedJSON = '';
+	options.path = config.ts.path.query;
+	options.headers.accept = 'application/sparql-results+json';
+	
+	var request = http.request(options, function (response) {
+		response.setEncoding('utf8');
+		response.on('data', function (chunk) {
+			returnedJSON += chunk;
+		});
+		response.on('end', function () {
+			var json = JSON.parse(returnedJSON);
+			callback(null, json);
+		});
+	});
 	request.on('error', function (e) {
 		callback(new Error(e.message));
 	});
