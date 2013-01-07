f3e010c8a49dab0f3ac138d1072cfba4ac886464
EivindEE <eivind.elseth@gmail.com>
2013-01-07 14:14:09 +0100

    Added pretty printing to html code

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.configure(function () {
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
+	app.configure('development', function(){
+		app.use(express.errorHandler());
+		app.locals.pretty = true;
+	});
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
@@ -22,7 +26,7 @@ app.configure(function () {
 	app.use(express.cookieParser('your secret here'));
 	app.use(express.session());
 	app.use(app.router);
-	app.use(require('stylus').middleware(__dirname + '/public'));
+//	app.use(require('stylus').middleware(__dirname + '/public'));
 	app.use(express.static(path.join(__dirname, 'public')));
 });
 
