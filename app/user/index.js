8246af701926b7a6ddb391326abc96521b71b3ff
John Fredrik Asphaug <john-f@online.no>
2013-02-15 14:53:59 +0100

    added instert into psql db

--- app/user/index.js
+++ app/user/index.js
@@ -1,13 +1,18 @@
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt'),
+pg = require('pg'),
+config = require('../config');
 
 
 exports.setUser = function (userData, callback) {
+	var con = config.postgresql,
+	client = new pg.Client(con);
+	client.connect();
 	bcrypt.genSalt(10, function(err, salt) {
 		bcrypt.hash(userData.password, salt, function (err, hash) {
 				if (err) {
 					console.log("error in bcrypt");
 				} else {
-					console.log(hash);
+				 client.query('INSERT INTO public.users(username, password, email) values($1, $2, $3)', [userData.name, hash, userData.email]);
 				}
 		});
 	});
