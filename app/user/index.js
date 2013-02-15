29fc20f0125975b0578eed44cc50cf44ff4fca06
John Fredrik Asphaug <john-f@online.no>
2013-02-15 15:51:56 +0100

    completed adding user to psql database. Added callback for error @

--- app/user/index.js
+++ app/user/index.js
@@ -4,15 +4,21 @@ config = require('../config');
 
 
 exports.setUser = function (userData, callback) {
-	var con = config.postgresql,
-	client = new pg.Client(con);
+	
+	var client = new pg.Client(config.psql);
 	client.connect();
 	bcrypt.genSalt(10, function(err, salt) {
 		bcrypt.hash(userData.password, salt, function (err, hash) {
 				if (err) {
 					console.log("error in bcrypt");
 				} else {
-				 client.query('INSERT INTO public.users(username, password, email) values($1, $2, $3)', [userData.name, hash, userData.email]);
+				 client.query('INSERT INTO users(username, password, email) values($1, $2, $3)', [userData.name, hash, userData.email], function (error, result) {
+				 		if(error) {
+				 			callback(error);
+				 		} else {
+				 			callback(null, result);
+				 		}
+				 });
 				}
 		});
 	});
