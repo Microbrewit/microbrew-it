b6f83550ad6a0c04c7867e9c975a0edafed3f4ef
Snorre Magnus Davøen <snorremd@gmail.com>
2013-07-08 20:01:33 +0200

    Added new files in route, one for each kind of resource (user, beer, brewery). Started implementing a user registration function in user part of routing.

--- app.js
+++ app.js
@@ -4,82 +4,94 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-	routes = require('./routes'),
-	http = require('http'),
-	path = require('path');
+ var express = require('express'),
+ RedisStore = require('connect-redis')(express),
+ routes = require('./routes'),
+ http = require('http'),
+ path = require('path');
 
-var app = express();
+ var app = express();
 
-app.configure(function () {
-	app.set('port', process.env.PORT || 3000);
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.configure('development', function () {
-		app.use(express.errorHandler());
-		app.locals.pretty = true;
-	});
-	app.use(express.favicon());
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser('your secret here'));
-	app.use(express.session());
-	app.use(app.router);
+ app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.configure('development', function () {
+        app.use(express.errorHandler());
+        app.locals.pretty = true;
+    });
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session(
+        // {
+        // store: new RedisStore({
+        //     host: 'localhost',
+        //     port: 6379,
+        //     db: 2,
+        //     pass: 'RedisPASS'
+        //     }),
+        // secret: '987654321'}
+    ));
+    app.use(app.router);
 
-	app.use(express.static(path.join(__dirname, 'public')));
-});
+    app.use(express.static(path.join(__dirname, 'public')));
+ });
 
-app.configure('development', function () {
-	app.use(express.errorHandler());
-});
+    app.configure('development', function () {
+        app.use(express.errorHandler());
+    });
 
-app.get('/', routes.index);
+    app.get('/', routes.index);
 
-// testing TODO: remove when app works
-app.get('/users', routes.list);
-app.get('/ask', routes.ask);
+app.get('/user/add', routes.user.addUser);
 
-
-// ================ FRONTEND ===============
-// users
-app.get('/user/:userName', routes.user);
-app.get('/add/user', routes.addUser);
-app.get('/login', routes.loginCheck);
-
-// searches
-app.get('/find/:searchTerms', routes.find);
-// app.get('/find/user/:name', routes.findUser);
-// app.get('/find/brewery/:name', routes.findBrewery);
-// app.get('/find/beer/:name', routes.findBeer);
-
-// beer and breweries
-app.get('/brewery/:brewery/:id', routes.brewery);
-app.get('/brewery/:id', routes.brewery);
-// app.get('/brewery/:brewery/:id/:beer', routes.beer);
-
-// add new beers and breweries
-// app.get('/add/brewery', routes.addBrewery);
-app.get('/add/beer', routes.addBeer);
+// // testing TODO: remove when app works
+// app.get('/users', routes.list);
+// app.get('/ask', routes.ask);
 
 
-// ======================= BACKEND =====================
-// TODO: naming scheme for API routes functions
-// app.get('/api/add/beer', routes.apiAddBeer);
-// app.get('/api/add/brewery', routes.apiAddBrewery);
-// app.get('/api/add/user', routes.apiAddUser);
-// app.get('/api/add/beer/rating', routes.apiAddBeerRating);
-// app.get('/api/add/brewery/rating', routes.apiAddBreweryRating);
-// app.get('/api/user/:userName', routes.apiUser);
-// app.get('/api/find/:searchTerms', routes.query);
-// app.get('/api/find/user/:name', routes.getBreweryName);
-// app.get('/api/find/brewery/:name', routes.getBreweryName);
-// app.get('/api/find/beer/:name', routes.getBeerName);
-// app.get('/api/brewery/:id', routes.breweryQuery);
-// app.get('/api/brewery/:brewery/:id', routes.breweryQuery);
-// app.get('/api/brewery/:brewery/:id/:beer', routes.beer);
+// // ================ FRONTEND ===============
+// // users
+// app.get('/user/:userName', routes.user);
+// app.get('/add/user', routes.addUser);
+// app.get('/login', routes.loginCheck);
 
-app.get('/BeerStyle', routes.beerStyleQuery);
+// // searches
+// app.get('/find/:searchTerms', routes.find);
+// // app.get('/find/user/:name', routes.findUser);
+// // app.get('/find/brewery/:name', routes.findBrewery);
+// // app.get('/find/beer/:name', routes.findBeer);
+
+// // beer and breweries
+// app.get('/brewery/:brewery/:id', routes.brewery);
+// app.get('/brewery/:id', routes.brewery);
+// // app.get('/brewery/:brewery/:id/:beer', routes.beer);
+
+// // add new beers and breweries
+// // app.get('/add/brewery', routes.addBrewery);
+// app.get('/add/beer', routes.addBeer);
+
+
+// // ======================= BACKEND =====================
+// // TODO: naming scheme for API routes functions
+// // app.get('/api/add/beer', routes.apiAddBeer);
+// // app.get('/api/add/brewery', routes.apiAddBrewery);
+// // app.get('/api/add/user', routes.apiAddUser);
+// // app.get('/api/add/beer/rating', routes.apiAddBeerRating);
+// // app.get('/api/add/brewery/rating', routes.apiAddBreweryRating);
+// // app.get('/api/user/:userName', routes.apiUser);
+// // app.get('/api/find/:searchTerms', routes.query);
+// // app.get('/api/find/user/:name', routes.getBreweryName);
+// // app.get('/api/find/brewery/:name', routes.getBreweryName);
+// // app.get('/api/find/beer/:name', routes.getBeerName);
+// // app.get('/api/brewery/:id', routes.breweryQuery);
+// // app.get('/api/brewery/:brewery/:id', routes.breweryQuery);
+// // app.get('/api/brewery/:brewery/:id/:beer', routes.beer);
+
+// app.get('/BeerStyle', routes.beerStyleQuery);
 
 
 // 404
@@ -87,5 +99,5 @@ app.use(function(req,res){
     res.render('404.jade');
 });
 http.createServer(app).listen(app.get('port'), function () {
-	console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
--- app/user/index.js
+++ app/user/index.js
@@ -5,46 +5,49 @@ config = require('../config');
 
 exports.setUser = function (userData, callback) {
 	
-	var client = new pg.Client(config.psql);
+	var client = new pg.Client("tcp://" + config.postgresql.user + ":" + config.postgresql.password + "@" + config.postgresql.host + ":" + config.postgresql.port + "/" + config.postgresql.database);
 	client.connect();
 	bcrypt.genSalt(10, function(err, salt) {
 		bcrypt.hash(userData.password, salt, function (err, hash) {
-				if (err) {
-					console.log("error in bcrypt");
-				} else {
-				 client.query('INSERT INTO users(username, password, email) values($1, $2, $3)', [userData.name, hash, userData.email], function (error, result) {
-				 		if(error) {
-				 			callback(error);
-				 		} else {
-				 			callback(null, result);
-				 		}
-				 });
-				}
+			if (err) {
+				console.log("error in bcrypt");
+				return callback(err);
+			} else {
+				client.query('INSERT INTO users(username, password, email) values($1, $2, $3)', [userData.name, hash, userData.email], function (error, result) {
+					if(error) {
+						console.log("Error in query");
+						callback(error);
+					} else {
+						console.log("Result: " + JSON.stringify(result));
+						callback(null, result);
+					}
+				});
+			}
 		});
 	});
 
 };
 
 exports.passwordCheck = function (userData, callback) {
-	var client = new pg.Client(config.psql);
+	var client = new pg.Client(config.postgresql);
 	client.connect();
 	client.query('SELECT password FROM users WHERE email = $1', [userData.email], function (err, res) {
 		if(err) {
 			callback(err);
 		} else {
 			if(res.rows.length > 0) {
-			var hash = res.rows[0].password;			
-			
-			bcrypt.compare(userData.password, hash, function (error, result) {
-				if(error) {
-					callback(error);
-				} else {
-					console.log(result);
-				}
-			});
-		} else {
-			callback(null, false);
-		}
+				var hash = res.rows[0].password;			
+
+				bcrypt.compare(userData.password, hash, function (error, result) {
+					if(error) {
+						callback(error);
+					} else {
+						console.log(result);
+					}
+				});
+			} else {
+				callback(null, false);
+			}
 		}
 
 	});
