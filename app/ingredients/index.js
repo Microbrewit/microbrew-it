71e02a0e3f990fea0dcc36166b636d07dfb48eb1
John Fredrik Asphaug <john-f@online.no>
2013-08-02 13:56:23 +0200

    Changed function in routes to take data from triplestore not mock data. Wrote function to query triplestore for name of spcific fermentable and return in json. Also added function to format SPARQL json result to api format

--- app/ingredients/index.js
+++ app/ingredients/index.js
@@ -7,11 +7,14 @@ var http = require('http'),
 
 
 var getFermentables = function (callback) {
-	var select = 	'SELECT DISTINCT ?fermentable ?label ?ppg ?colour ?type ?typeuri ';
-		select += 	'WHERE { ' ;
-		select +=	'?typeuri rdfs:subClassOf ' + mb.fermentables + '; rdfs:label ?type . ?fermentable rdf:type ?typeuri; rdfs:label ?label; ' + mb.hasPPG + ' ?ppg; ' + mb.hasColour + ' ?colour . ';
-		select += 	'FILTER(?typeuri != ' + mb.fermentables + ') . ';
-		select +=	'FILTER(LANG(?type) = "en") . }';
+	var select = 	' SELECT DISTINCT ?fermentable ?label ?ppg ?colour ?type ?typeuri ?supplier ?supplieruri';
+		select += 	' WHERE { ' ;
+		select +=	' ?typeuri rdfs:subClassOf ' + mb.fermentables + '; rdfs:label ?type . ?fermentable a ?typeuri; rdfs:label ?label; ';
+		select +=	mb.hasPPG + ' ?ppg; ' + mb.hasColour + ' ?colour .';
+		select +=	' OPTIONAL {?fermentable ' + mb.suppliedBy + ' ?supplieruri . ';
+		select +=	' ?supplieruri rdfs:label ?supplier . FILTER(LANG(?supplier) ="en") . } ';
+		select += 	' FILTER(?typeuri != ' + mb.fermentables + ') . FILTER(?typeuri != ' + mb.extract + ')';
+		select +=	' FILTER(LANG(?type) = "en") . }';
 	console.log(select);
 		ts.select(select, function (err, result) {
 			if(err) {
@@ -19,13 +22,88 @@ var getFermentables = function (callback) {
 				callback(err);
 			} else {
 
-				callback(null, util.apiFormatting(result));
+				callback(null, apiFormattingFermentables(result));
 			}
 		});
 };
 
+var getFermentable = function (ferm, callback) {
+	console.log(ferm);
+	var select =	' SELECT ?fermentable ?label ?ppg ?colour ?type ?typeuri ?supplier ?supplieruri';
+		select +=	' WHERE { ';
+		select +=	' ?fermentable rdfs:label "' + ferm + '"@en; rdfs:label ?label; rdf:type ?typeuri; ';
+		select +=	mb.hasPPG + ' ?ppg; ' + mb.hasColour + ' ?colour . ';
+		select +=	' OPTIONAL { ?fermentable' + mb.suppliedBy + ' ?supplieruri . ?supplieruri rdfs:label ?supplier . FILTER(LANG(?supplier) ="en") . } ';
+		select +=	' ?typeuri rdfs:label ?type . FILTER(LANG(?type) = "en") . ';
+		select +=	' FILTER(?typeuri != ' + mb.fermentables + ') . FILTER(?typeuri != owl:NamedIndividual) .';
+		select +=	' FILTER(?typeuri != rdfs:Resource) . FILTER(?typeuri != ' + mb.ingredient +  ') . ';
+		select +=	' FILTER(LANG(?label) = "en") . ';
+		select +=	' }'   	
+	console.log(select);
+		ts.select(select, function (err, result) {
+			if(err) {
+				console.log(err);
+				callback(err);
+			} else {
+
+				callback(null, apiFormattingFermentables(result));
+			}
+		});
+
+
+};
+
+var apiFormattingFermentables = function (result) {
+	console.log(result);
+	var apiJson = {
+			'meta': {
+			'size':	result.results.bindings.length
+			},
+			'links': {
+				'fermentables.maltster': {
+					'href': 'http://api.microbrew.it/maltsters/:maltsterid/',
+    				'type': 'maltster'
+				}
+
+			},
+			'fermentables' :[
+			]
+
+
+	};
+	for (var i = 0; i < result.results.bindings.length; i++) {
+		if(result.results.bindings[i].supplieruri != null) {
+			apiJson.fermentables[i] = {
+				'id': result.results.bindings[i].fermentable.value,
+				'href': result.results.bindings[i].fermentable.value,
+				'fermentablename': result.results.bindings[i].label.value,
+				'colour': result.results.bindings[i].colour.value,
+				'ppg' : result.results.bindings[i].ppg.value,
+				'type': result.results.bindings[i].type.value,
+				'maltster': result.results.bindings[i].supplier.value,
+				'links': {
+				'maltsterid': result.results.bindings[i].supplieruri.value
+				}
+
+			}
+
+		} else {
+			apiJson.fermentables[i] = {
+				'id': result.results.bindings[i].fermentable.value,
+				'href': result.results.bindings[i].fermentable.value,
+				'fermentablename': result.results.bindings[i].label.value,
+				'colour': result.results.bindings[i].colour.value,
+				'ppg' : result.results.bindings[i].ppg.value,
+				'type': result.results.bindings[i].type.value
+				
+			}
+		}
+	}
+	return apiJson;
+};
 
 exports = module.exports = {
-	'getFermentables': getFermentables
+	'getFermentables': getFermentables,
+	'getFermentable': getFermentable
 
 };
--- routes/ingredients.js
+++ routes/ingredients.js
@@ -1,17 +1,59 @@
 var url = require('url'),
 utils = require('../app/util'),
-mock = require('../app/mock');
+mock = require('../app/mock'),
+ingredients = require('../app/ingredients');
 
 var fermentables = function (req, res) {
-	res.writeHead(200, {'Content-Type' : 'application/json'});
-    res.end(JSON.stringify(mock.fermentables));
+	ingredients.getFermentables( function (error, result) {
+		if(error) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(error.message);
+		} else {
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(JSON.stringify(result));
+			}
+	});
 };
 
 var fermentable = function (req, res) {
-	res.writeHead(200, {'Content-Type' : 'application/json'});
-    res.end(JSON.stringify(mock.fermentable));
-}
+	var ferm = req.params.fermentable;
+	ingredients.getFermentable(ferm, function (error, result) {
+		if(error) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(error.message);
+		} else {
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(JSON.stringify(result));
+			}
+	});
+};
 
+var apiFormattingFermentables = function (result) {
+	console.log(result);
+	var apiJson = {
+			'meta': {
+			'size':	result.results.bindings.length
+			},
+			'links': {
+
+			},
+			'fermentables' :[
+			]
+
+
+	};
+	for (var i = 0; i < result.results.bindings.length; i++) {
+		apiJson.fermentables[i] = {
+			'id': result.results.bindings[i].fermentable.value,
+			'href': result.results.bindings[i].fermentable.value,
+			'label': result.results.bindings[i].label.value,
+			'colour': result.results.bindings[i].colour.value,
+			'ppg' : result.results.bindings[i].ppg.value,
+			'type': result.results.bindings[i].type.value
+		}
+	}
+	return apiJson;
+};
 
 exports = module.exports = {
 	'fermentables': fermentables,
