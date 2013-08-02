e0a68ddca4b9f3af2f4d41b60ae662923711062f
John Fredrik Asphaug <john-f@online.no>
2013-08-02 16:00:47 +0200

    Added routes for hop and hops

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -28,34 +28,34 @@ var fermentable = function (req, res) {
 	});
 };
 
-var apiFormattingFermentables = function (result) {
-	console.log(result);
-	var apiJson = {
-			'meta': {
-			'size':	result.results.bindings.length
-			},
-			'links': {
-
-			},
-			'fermentables' :[
-			]
-
-
-	};
-	for (var i = 0; i < result.results.bindings.length; i++) {
-		apiJson.fermentables[i] = {
-			'id': result.results.bindings[i].fermentable.value,
-			'href': result.results.bindings[i].fermentable.value,
-			'label': result.results.bindings[i].label.value,
-			'colour': result.results.bindings[i].colour.value,
-			'ppg' : result.results.bindings[i].ppg.value,
-			'type': result.results.bindings[i].type.value
-		}
-	}
-	return apiJson;
+var hops = function (req, res) {
+	ingredients.getHops( function (error, result) {
+		if(error) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(error.message);
+		} else {
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(JSON.stringify(result));
+			}
+	});
 };
 
+var hop = function (req, res) {
+	var hop = req.params.hop;
+	console.log(hop);
+	ingredients.getHop(hop, function (error, result) {
+		if(error) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(error.message);
+		} else {
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(JSON.stringify(result));
+			}
+	});
+};
 exports = module.exports = {
 	'fermentables': fermentables,
-	'fermentable': fermentable
+	'fermentable': fermentable,
+	'hops': hops,
+	'hop': hop
 };
