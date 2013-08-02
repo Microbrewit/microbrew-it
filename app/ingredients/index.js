1c8f0f31ed65a60fc4cb8e145728e7ddef847f14
John Fredrik Asphaug <john-f@online.no>
2013-08-02 17:36:54 +0200

    added apijson formatting for hops and hop.

--- app/ingredients/index.js
+++ app/ingredients/index.js
@@ -33,8 +33,8 @@ var getFermentable = function (ferm, callback) {
 	var select =	' SELECT ?fermentable ?label ?ppg ?colour ?type ?typeuri ?supplier ?supplieruri';
 		select +=	' WHERE { ';
 		select +=	' <'+ ferm + '> rdfs:label ?label; rdf:type ?typeuri; rdf:type ' + mb.fermentables + ';' ;
-		select +=	mb.hasPPG + ' ?ppg; ' + mb.hasColour + ' ?colour . ';
-		select +=	' OPTIONAL { ?fermentable' + mb.suppliedBy + ' ?supplieruri . ?supplieruri rdfs:label ?supplier . FILTER(LANG(?supplier) ="en") . } ';
+		select +=	mb.hasPPG + ' ?ppg; ' + mb.hasColour + ' ?colour . ?fermentable rdfs:label ?label ';
+		select +=	' OPTIONAL {<'+ ferm + '> ' + mb.suppliedBy + ' ?supplieruri . ?supplieruri rdfs:label ?supplier . FILTER(LANG(?supplier) ="en") . } ';
 		select +=	' ?typeuri rdfs:label ?type . FILTER(LANG(?type) = "en") . ';
 		select +=	' FILTER(?typeuri != ' + mb.fermentables + ') . FILTER(?typeuri != owl:NamedIndividual) .';
 		select +=	' FILTER(?typeuri != rdfs:Resource) . FILTER(?typeuri != ' + mb.ingredient +  ') . ';
@@ -80,7 +80,7 @@ var apiFormattingFermentables = function (result) {
 			apiJson.fermentables[i] = {
 				'id': result.results.bindings[i].fermentable.value,
 				'href': result.results.bindings[i].fermentable.value,
-				'fermentablename': result.results.bindings[i].label.value,
+				'name': result.results.bindings[i].label.value,
 				'colour': result.results.bindings[i].colour.value,
 				'ppg' : result.results.bindings[i].ppg.value,
 				'type': result.results.bindings[i].type.value,
@@ -107,12 +107,12 @@ var apiFormattingFermentables = function (result) {
 };
 
 var getHops = function (callback) {
-	var select = 	' SELECT DISTINCT ?hop ?label ?alphaacid ?recommendedUsage ?flavorDescription ?origin';
+	var select = 	' SELECT *';
 		select += 	' WHERE { ' ;
 		select +=	' ?hop rdf:type ' + mb.hops + '; rdfs:label ?label ; ';
-		select +=	mb.hasAlphaAcid + '?alphaacid ; ' + mb.recommendedUsage +' ?recommendedUsage;';
-		select +=	mb.origin + '?origin . ';
-		select += 	' OPTIONAL {?hop ' + mb.flavorDescription +  ' ?flavorDescription . }';
+		select +=	mb.hasAlphaAcid + '?alphaacid ; ' + mb.recommendedUsage +' ?recommendedUsageid;';
+		select +=	mb.origin + '?originid; ' + mb.flavorDescription +  ' ?flavorDescription . ';
+		select += 	'?originid rdfs:label ?origin . ?recommendedUsageid rdfs:label ?recommendedUsage ';
 		select +=	' FILTER(LANG(?label) = "en") . }';
 	console.log(select);
 		ts.select(select, function (err, result) {
@@ -121,19 +121,19 @@ var getHops = function (callback) {
 				callback(err);
 			} else {
 
-				callback(null, result);
+				callback(null, apiFormattingHops(result));
 			}
 		});
 };
 
 var getHop = function (hop, callback) {
 	if(hop.indexOf(mb.baseURI) != -1 ) { 
-	var select = 	' SELECT DISTINCT ?hop ?label ?alphaacid ?recommendedUsage ?flavorDescription ?origin';
+	var select = 	' SELECT *';
 		select +=	' WHERE { ';
 		select +=	'<' + hop + '> rdf:type ' + mb.hops + '; rdfs:label ?label ; ';
-		select +=	mb.hasAlphaAcid + '?alphaacid ; ' + mb.recommendedUsage +' ?recommendedUsage;';
-		select +=	mb.origin + '?origin . ';
-		select += 	' OPTIONAL {<' + hop + '> ' + mb.flavorDescription +  ' ?flavorDescription . }';
+		select +=	mb.hasAlphaAcid + '?alphaacid ; ' + mb.recommendedUsage +' ?recommendedUsageid;';
+		select +=	mb.origin + '?originid ; ' + mb.flavorDescription +  ' ?flavorDescription . ?hop rdfs:label ?label . ';
+		select +=	' ?recommendedUsageid rdfs:label ?recommendedUsage . ?originid rdfs:label ?origin'
 		select +=	' FILTER(LANG(?label) = "en") . }';
 		console.log(select);
 		ts.select(select, function (err, result) {
@@ -142,13 +142,53 @@ var getHop = function (hop, callback) {
 				callback(err);
 			} else {
 
-				callback(null, result);
+				callback(null, apiFormattingHops(result));
 			}
 		});
 	} else {
 		console.log('ERROR');
 		callback(null, 'Not a Hops');
 	}
+};
+
+var apiFormattingHops = function (result) {
+	var apiJson = {
+			'meta': {
+			'size':	result.results.bindings.length
+			},
+			'links': {
+				'hops.maltster': {
+					'href': 'http://api.microbrew.it/hops/recommendeduses/:recommendeduseid',
+    				'type': 'recommendedUses'
+				},
+				'hops.origin': {
+   					'href': 'http://api.microbrew.it/origin/:originid',
+   					'type': 'origin'
+  				},
+
+			},
+			'hops' :[
+			]
+
+
+	};
+
+	for (var i = 0; i < result.results.bindings.length; i++) {
+		apiJson.hops[i] = {
+				'id': result.results.bindings[i].hop.value,
+				'href': result.results.bindings[i].hop.value,
+				'name': result.results.bindings[i].label.value,
+				'alphaacid': result.results.bindings[i].alphaacid.value,
+				'flavor' : result.results.bindings[i].flavorDescription.value,
+				'origin' : result.results.bindings[i].origin.value,
+				'recommendedUsage' : result.results.bindings[i].recommendedUsage.value,
+				'links': {
+				'originid': result.results.bindings[i].originid.value,
+				'recommendedUsage': result.results.bindings[i].recommendedUsageid.value			
+			}
+		}
+	}
+	return apiJson;
 };
 
 exports = module.exports = {
