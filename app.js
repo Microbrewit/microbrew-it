a45affa97c26fe846b8c2cfe8177ccf4cd2e5c6f
John Fredrik Asphaug <john-f@online.no>
2013-08-05 14:51:57 +0200

    added CORS support for all

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
  var app = express();
 
  var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
