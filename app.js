3d2c23d5c9976b9f64584bcd9841542570973b96
John Fredrik Asphaug <john-f@online.no>
2013-08-05 15:03:04 +0200

    refactored two res.header into one

--- app.js
+++ app.js
@@ -15,8 +15,7 @@
  var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Headers': 'X-Requested-With, Content-Type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
 
     next();
 }
