85c10dc04c85a609362d582b771694f0fbe77165
John Fredrik Asphaug <john-f@online.no>
2013-08-05 15:01:14 +0200

    fixed bug

--- app.js
+++ app.js
@@ -16,7 +16,7 @@
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.heaxer('Access-Control-Allow-Headers': 'X-Requested-With, Content-Type');
+    res.header('Access-Control-Allow-Headers': 'X-Requested-With, Content-Type');
 
     next();
 }
