99b7459aab68306af5b9a2715fad87b93b4f8d13
John Fredrik Asphaug <john-f@online.no>
2013-08-13 15:23:17 +0200

    fixed typo and added selectSync syncronouse function for select queries

--- app/triplestore/index.js
+++ app/triplestore/index.js
@@ -93,24 +93,28 @@ exports.select = function (query, callback) {
 };
 
 exports.selectSync = function (query) {
-	var options = until.beget(opts),
+	console.log('selectSync');
+	var options = util.beget(opts),
 		returnedJSON = '',
 		request;
 
-	option.path = config.ts.path.query;
+	options.path = config.ts.path.query;
 	options.headers.accept = 'application/sparql-results+json';
 	request = http.request(options, function (response) {
 		response.setEncoding('utf8');
 		response.on('data', function (chunk) {
+			console.log('chunk:' + chunk);
 			returnedJSON += chunk;
 		});
 		response.on('end', function () {
 			var json = JSON.parse(returnedJSON);
-			return(null, json);
+			console.log(json);
+			return(json);
 		});
 	});
 	request.on('error', function (e) {
-		console.log(new Error(e.message));
+		console.error('error');
+    	console.error(e);
 	});
 	request.end('query=' + encodeURIComponent(query));
-}
+};
