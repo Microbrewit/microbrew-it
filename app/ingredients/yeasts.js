274078f784556f5ccd9cce63af5d2a51adb9e06a
John Fredrik Asphaug <john-f@online.no>
2013-08-22 23:00:22 +0200

    Added funciton to get single yeast as well as getting dry yeast and liquid yeast from triplestore

--- app/ingredients/yeasts.js
+++ app/ingredients/yeasts.js
@@ -2,23 +2,66 @@
 var http = require('http'),
     config = require('../config'),
     mb = require('../ontology').mb,
+    async = require('async'),
     ts = require('../triplestore');
 
+var apiFormattingYeasts = function (yeastGraph, callback) {
+
+	var yeastArray = [],
+	j = 0;
+	for(var key in yeastGraph){
+		yeastArray.push({
+			'id': yeastGraph[key][mb.baseURI + 'hasID'][0].value,
+			'href': key,
+			'name': yeastGraph[key]['http://www.w3.org/2000/01/rdf-schema#label'][0].value,
+			'suppliedby': yeastGraph[key][mb.baseURI + 'suppliedBy'][0].value,
+			'comment': yeastGraph[key]['http://www.w3.org/2000/01/rdf-schema#comment'][0].value
+	});
+
+	j++
+	}
+	callback(null, yeastArray);
+
+};
+
 var getYeasts = function (callback) {
-    var select =    'SELECT DISTINCT *';
-        select +=   ' WHERE { ';
-        select +=   '?typeuri rdfs:subClassOf ' + mb.yeast + ' ; rdfs:label ?type . ?yeast a ?typeuri; rdfs:label ?yeastname .';
-        select +=	'FILTER(?typeuri != ' + mb.yeast + ' ) .'
-        select +=   '}';
-        console.log(select);
-        ts.select(select, function (err, result) {
-            if(err) {
-                console.log(err);
-                callback({'statuscode': 500, 'error': err});
-            } else {
-                callback(null, {'statuscode': 200, 'result': result});
-            }
-        });
+	async.parallel({
+		liquidyeasts : function(callback) {
+			getLiquidYeasts(function (error, result) {
+				if(error) {
+					callback(error);
+				} else {
+					callback(null, result);
+				}
+			});
+		},
+		dryyeasts : function(callback) {
+			getDryYeasts(function (error, result) {
+				if(error) {
+					callback(error);
+				} else {
+					callback(null, result);
+				}
+			});
+		}
+	}, function (error,result) {
+		if(error) {
+			callback(error);
+		} else {
+			console.log(result.dryyeasts.length)
+		var apiJson = {
+        	'meta': {
+          		'size': result.dryyeasts.length + result.liquidyeasts.length
+        		},
+        	'links': {
+
+          	},
+      		};
+		apiJson.yeasts = result;
+			callback(null,apiJson);
+		}
+	});
+
 };
 
 var getYeast = function (yeast, callback) {
@@ -39,7 +82,43 @@ var getYeast = function (yeast, callback) {
 		});
 };
 
+var getLiquidYeasts = function (callback) {
+	ts.graph('<' + mb.baseURI + 'Liquid_Yeast>', function (error, liquidYeastGraph) {
+		if(error) {
+			callback(error);
+		} else {
+			apiFormattingYeasts(liquidYeastGraph, function (err, res) {
+				if(err) {
+					callback(err);
+				} else {
+					callback(null, res);
+				}
+			});
+		//callback(null, liquidYeastGraph);
+		}
+	});
+}
+
+var getDryYeasts = function (callback) {
+	ts.graph('<' + mb.baseURI + 'Dry_Yeast>', function (error, dryYeastGraph) {
+		if(error) {
+			callback(error);
+		} else {
+			apiFormattingYeasts(dryYeastGraph, function (err, res) {
+				if(err) {
+					callback(err);
+				} else {
+					callback(null, res);
+				}
+			});
+		//callback(null, liquidYeastGraph);
+		}
+	});
+}
+
 exports = module.exports = {
     'getYeasts': getYeasts,
-    'getYeast': getYeast
+    'getYeast': getYeast,
+    'getLiquidYeasts': getLiquidYeasts,
+    'getDryYeasts': getDryYeasts,
 };
