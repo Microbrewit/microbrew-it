fa385c7964034997b9f9d737a6fcaeb0c2fdc38a
John Fredrik Asphaug <john-f@online.no>
2013-08-23 13:23:34 +0200

    Added support for JSONP

--- routes/yeasts.js
+++ routes/yeasts.js
@@ -18,10 +18,10 @@ var yeastList = function(req, res) {
             res.writeHead(head.statuscode, head.contentType);
             res.end(JSON.stringify(response));
         } else {
-            head.statuscode = 200;
-            res.writeHead(head.statuscode, head.contentType);
-            res.end(JSON.stringify(result));
-        }
+			response = utils.formatJsonResponse(req.query, result);
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(response);
+			}
     });
 };
 
@@ -40,10 +40,10 @@ var yeast = function(req, res) {
             res.writeHead(head.statuscode, head.contentType);
             res.end(JSON.stringify(response));
         } else {
-            head.statuscode = 200;
-            res.writeHead(head.statuscode, head.contentType);
-            res.end(JSON.stringify(result));
-        }
+			response = utils.formatJsonResponse(req.query, result);
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(response);
+			}
     });
 };
 
@@ -53,8 +53,23 @@ var liquidYeasts = function (req, res) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.end(error.message);
 		} else {
+			response = utils.formatJsonResponse(req.query, result);
 			res.writeHead(200, {'Content-Type' : 'application/json'});
-    		res.end(JSON.stringify(result));
+    		res.end(response);
+			}
+	});
+};
+
+var liquidYeast = function (req, res) {
+	var liquidID = req.params.liquidyeast;
+	yeasts.getLiquidYeast(liquidID, function (error, result) {
+		if(error) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(error.message);
+		} else {
+			response = utils.formatJsonResponse(req.query, result);
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(response);
 			}
 	});
 };
@@ -65,15 +80,33 @@ var dryYeasts = function (req, res) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.end(error.message);
 		} else {
+			response = utils.formatJsonResponse(req.query, result);
 			res.writeHead(200, {'Content-Type' : 'application/json'});
-    		res.end(JSON.stringify(result));
+    		res.end(response);
+			}
+		});
+};
+
+var dryYeast = function (req, res) {
+	var dryID = req.params.dryyeast;
+	yeasts.getDryYeast(dryID, function (error, result) {
+		if(error) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(error.message);
+		} else {
+			response = utils.formatJsonResponse(req.query, result);
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+    		res.end(response);
 			}
 	});
 };
+
 
 exports = module.exports = {
     'yeasts': yeastList,
     'yeast' : yeast,
     'liquidYeasts' : liquidYeasts,
+    'liquidYeast': liquidYeast,
     'dryYeasts' : dryYeasts,
+    'dryYeast': dryYeast,
 };
