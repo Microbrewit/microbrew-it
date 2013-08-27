9c34e4cb2c4042442964f028261e7cb048d42532
John Fredrik Asphaug <john-f@online.no>
2013-08-27 11:08:10 +0200

    Added routes for beerstyles

--- routes/beer.js
+++ routes/beer.js
@@ -1,3 +1,26 @@
-exports.add = function () {
-	console.log("Add beer");
+(function(){
+  'use strict';
+var util = require('../app/util'),
+    beer = require('../app/beer');
+
+var beerStyles = function (req, res) {
+	beer.getBeerStyles( function (error, result) {
+		if(error) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end(error.message);
+		} else {
+		var response = util.formatJsonResponse(req.query, result);
+			res.writeHead(200, {'Content-Type' : 'application/json'});
+			res.end(response);
+			}
+	});
 };
+
+
+exports = module.exports = {
+	'beerStyles':beerStyles,
+
+};
+
+})();
+
