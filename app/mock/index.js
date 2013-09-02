1a72b9a4d2942d19823f6fc3d31c26e145fd018c
John Fredrik Asphaug <john-f@online.no>
2013-09-02 13:31:42 +0200

    Adeed more mock data

--- app/mock/index.js
+++ app/mock/index.js
@@ -1,4 +1,118 @@
 
+exports.recipe = {
+		'user': [{
+			'id': 1377263864213,
+			'href': 'http://microbrew.it/users/torstein'
+		}],
+		'mashSteps' :[
+			{
+				'number': 1,
+				'type': 'http://microbrew.it/ontology.owl#Infusion',
+				'length': 60,
+				'volume': 20,
+				'temperature': 65,
+				'fermentables': [
+					{
+						'id': '1377073452234',
+						'href': 'http://microbrew.it/ontology.owl#Boortmalt_Amber_Malt',
+						'name': 'Amber Malt',
+						'colour': '20',
+						'ppg': '34',
+						'suppliedbyid': 'http://microbrew.it/ontology.owl#Boortmalt',
+						'origin': 'Germany',
+						'suppliedBy': 'Boortmalt',
+						'amount': 20
+					},
+					{
+						'id': '1377073452234',
+						'href': 'http://microbrew.it/ontology.owl#mb:Simpsons_Pale_Ale_Malt',
+						'name': 'Pale Ale Malt',
+						'amount': 50
+					},
+
+				],
+				'hops': [],
+				'spices': [],
+				'fruits': [],
+				'notes': ''
+			}
+		],
+
+		'boilSteps': [
+			{
+				'number': 2,
+				'length': 60,
+				'volume': 20,
+				'fermentables': [],
+				'hops': [
+					{
+						'href': 'http://microbrew.it/ontology#Summit',
+						'aalow': 5,
+						'amount': 50
+					},
+					{
+						'href': 'http://microbrew.it/ontology#Willamette',
+						'aalow': 5,
+						'amount': 50
+					},
+				],
+				'spices': [],
+				'fruits': [],
+				'notes': ''
+			},
+			{
+				'number': 4,
+				'length': 0,
+				'volume': 20,
+				'fermentables': [],
+				'hops': [],
+				'spices': [],
+				'fruits': [
+					{
+					'href':'http://microbrew.it/ontology#Strawberry',
+					'amount': 30
+					}
+				],
+				'notes': ''
+			}
+		],
+
+		'fermentationSteps' : [
+			{
+				'number': 5,
+				'type': 'http://microbrew.it/ontology#Primary',
+				'length': 14,
+				'temperature': 24,
+				'fermentables': [],
+				'hops': [
+					{
+						'href': 'http://microbrew.it/ontology#Summit',
+						'aalow': 5,
+						'amount': 50
+					},
+					{
+						'href': 'http://microbrew.it/ontology#Willamette',
+						'aalow': 5,
+						'amount': 50
+					},
+
+				],
+				'spices': [],
+				'fruits': [],
+				'yeasts': [],
+				'notes': ''
+			}
+		],
+
+		'bottling' : [
+			{
+				'fermentables': []
+			},
+		],
+
+		'notes': '',
+	};
+
 exports.fermentables = {
                       'meta': {
                         'total': 123608,
